4e349d0d632796aa4177534601920a49ae074536
Lucian Leustean <lucian.leustean@gmail.com>
2018-01-04 21:34:13 +0200

    Add Index page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import MainPage from './pages/MainPage'
+import IndexPage from './pages/IndexPage'
 
 ReactDOM.render(
   <BrowserRouter>
     <div>
-      <Route path='/' component={MainPage} />
+      <Route path="/" component={MainPage}/>
+      <Route exact path="/" component={IndexPage}/>
     </div>
   </BrowserRouter>,
   document.getElementById('app-container')
